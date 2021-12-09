b9e636bc40e5115487189a3e5befd7c01cd57b9d
Justin Park <jmpark@email.wm.edu>
2021-12-09 18:12:47 -0500

    Created MazePanel base class and moved constants class over from eclipse

---
+++ app/src/main/java/edu/wm/cs/cs301/JustinPark/gui/MazePanel.java
@@ -0,0 +1,10 @@
+package edu.wm.cs.cs301.JustinPark.gui;
+
+import android.content.Context;
+import android.view.View;
+
+public class MazePanel extends View {
+    public MazePanel(Context context) {
+        super(context);
+    }
+}
