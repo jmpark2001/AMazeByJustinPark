fd51430fbf37a1408f84825f3bc4c7082e1e393b
Justin Park <jmpark@email.wm.edu>
2021-12-10 04:13:23 -0500

    Imported classes from eclipse and changed implementations

---
+++ app/src/main/java/edu/wm/cs/cs301/JustinPark/generation/Singleton.java
@@ -0,0 +1,11 @@
+package edu.wm.cs.cs301.JustinPark.generation;
+
+import edu.wm.cs.cs301.JustinPark.gui.StatePlaying;
+
+public class Singleton {
+    public static Maze mazeConfig;
+    public static StatePlaying state;
+    public static boolean isOutside;
+    public static boolean run;
+    public static String path;
+}
