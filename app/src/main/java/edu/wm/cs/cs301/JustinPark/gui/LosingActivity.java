e18ced55eabbb89c57094bc951f438ac9b99264f
Justin Park <jmpark@email.wm.edu>
2021-12-11 07:36:38 -0500

    Added music to losing screen

--- app/src/main/java/edu/wm/cs/cs301/JustinPark/gui/LosingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/JustinPark/gui/LosingActivity.java
@@ -3,6 +3,7 @@ package edu.wm.cs.cs301.JustinPark.gui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,10 +12,17 @@ import edu.wm.cs.cs301.JustinPark.R;
 public class LosingActivity extends AppCompatActivity {
     int pathLength = 0;
     TextView lose, energyUsed, path;
+    private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_losing);
+
+        mediaPlayer = MediaPlayer.create(LosingActivity.this, R.raw.silent);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+
         Intent intent = getIntent();
         pathLength = intent.getIntExtra(PlayManuallyActivity.PATH_LENGTH, 0);
         lose = (TextView) findViewById(R.id.textView7);
@@ -22,5 +30,24 @@ public class LosingActivity extends AppCompatActivity {
         path = (TextView) findViewById(R.id.textView12);
         energyUsed.setText("Energy Comsumption: 3500");
         path.setText("Path Length: " + pathLength);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
     }
 }
